41f6bfe74e9a3b293ca272208234ff4ca903de94
REÇ <ecoban@sutas.com.tr>
2018-12-21 18:14:48 +0300

    first template

---
+++ src/modules/flex/flex.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './flex.scss';
+
+const flex = () => {
+    return (
+        <div className="container">
+            <div className="item">1</div>
+            <div className="item">2</div>
+            <div className="item">3</div>
+            <div className="item">4</div>
+            <div className="item">5</div>
+        </div>
+    );
+}
+
+export default flex;
