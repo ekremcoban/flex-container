9589d051459d6b80ab3cd7feda73334152c57a32
unknown <resulekremcoban@gmail.com>
2018-12-23 14:21:27 +0200

    last template

--- src/modules/flex/flex.js
+++ src/modules/flex/flex.js
@@ -5,10 +5,15 @@ const flex = () => {
     return (
         <div className="container">
             <div className="item">1</div>
-            <div className="item">2</div>
+            <div className="item i2">2</div>
             <div className="item">3</div>
-            <div className="item">4</div>
+            <div className="item i4">4</div>
             <div className="item">5</div>
+            <div className="item">6</div>
+            <div className="item">7</div>
+            <div className="item">8</div>
+            <div className="item">9</div>
+            <div className="item">10</div>
         </div>
     );
 }
